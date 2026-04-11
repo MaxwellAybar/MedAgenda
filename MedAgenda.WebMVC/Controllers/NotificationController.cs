7fa80936a2f217744c81534f6c71bd8f91268109
Tu Nombre <tuemail@example.com>
2026-04-10 23:21:01 -0800

    Controller

---
+++ MedAgenda.WebMVC/Controllers/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using MedAgenda.WebMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedAgenda.WebMVC.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly NotificationService _service;
+
+        public NotificationController(NotificationService service)
+        {
+            _service = service;
+        }
+
+       
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAllAsync();
+            return View(data);
+        }
+    }
+}
