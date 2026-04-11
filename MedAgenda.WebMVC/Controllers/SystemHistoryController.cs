3ca5812462a19c88ad60c204b65b2e73cc286b87
Tu Nombre <tuemail@example.com>
2026-04-10 21:25:17 -0800

    Controllers

---
+++ MedAgenda.WebMVC/Controllers/SystemHistoryController.cs
@@ -0,0 +1,21 @@
+﻿using MedAgenda.WebMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedAgenda.WebMVC.Controllers
+{
+    public class SystemHistoryController : Controller
+    {
+        private readonly SystemHistoryService _service;
+
+        public SystemHistoryController(SystemHistoryService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAllAsync();
+            return View(data);
+        }
+    }
+}
---
+++ MedAgenda.WebMVC/Controllers/SystemReportsController.cs
@@ -0,0 +1,21 @@
+﻿using MedAgenda.WebMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedAgenda.WebMVC.Controllers
+{
+    public class SystemReportsController : Controller
+    {
+        private readonly SystemReportsService _service;
+
+        public SystemReportsController(SystemReportsService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAllAsync();
+            return View(data);
+        }
+    }
+}
