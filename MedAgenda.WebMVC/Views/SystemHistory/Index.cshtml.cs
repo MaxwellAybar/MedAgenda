108e32c4ef43511353a8c4d65cc64800e805638d
Tu Nombre <tuemail@example.com>
2026-04-10 21:42:39 -0800

    Views

---
+++ MedAgenda.WebMVC/Views/SystemHistory/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MedAgenda.WebMVC.Views.SystemHistory
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
